6670d5bb20cbea93b7e375074a6e6dadafa28132
DuyDung <dinhvandung791@gmail.com>
2020-05-28 00:48:26 +0700

    data jpa and mysql phan 4

--- spring-mvc/src/main/java/com/laptrinhjavaweb/dao/impl/NewDAO.java
+++ spring-mvc/src/main/java/com/laptrinhjavaweb/dao/impl/NewDAO.java
@@ -2,21 +2,18 @@ package com.laptrinhjavaweb.dao.impl;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.laptrinhjavaweb.dao.INewDAO;
 import com.laptrinhjavaweb.mapper.NewMapper;
 import com.laptrinhjavaweb.model.NewModel;
 
+@Repository
 public class NewDAO extends AbstractDAO<NewModel> implements INewDAO {
 
 	@Override
 	public List<NewModel> finldAll( ) {
 		StringBuilder sql = new StringBuilder("select * from news");
-		if(pageble.getSorter() != null) {
-			sql.append(" order by "+pageble.getSorter().getSortName()+" "+pageble.getSorter().getSortBy());
-		}
-		if(pageble.getOffset() !=null && pageble.getLimit() != null) {
-			sql.append(" LIMIT "+pageble.getOffset()+","+pageble.getLimit());
-		}
 		return query(sql.toString(), new NewMapper());
 	}		
 }
---
+++ spring-mvc/src/main/java/com/laptrinhjavaweb/entity/NewEntity.java
@@ -0,0 +1,8 @@
+package com.laptrinhjavaweb.entity;
+
+import javax.persistence.Table;
+
+@Table
+public class NewEntity {
+
+}
