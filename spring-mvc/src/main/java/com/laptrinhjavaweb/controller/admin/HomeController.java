fcf43c09d92153616747192527911696e4263598
DuyDung <dinhvandung791@gmail.com>
2020-08-26 23:10:01 +0700

    import package another project java

--- spring-mvc/src/main/java/com/laptrinhjavaweb/controller/admin/HomeController.java
+++ spring-mvc/src/main/java/com/laptrinhjavaweb/controller/admin/HomeController.java
@@ -1,12 +1,18 @@
 package com.laptrinhjavaweb.controller.admin;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.laptrinhjavaweb.service.*;
+
 @Controller(value = "homeControllerOfAdmin")
 public class HomeController {
+	
+	@Autowired
+	IProductService productService;
 	
 	@RequestMapping(value = "/quan-tri/trang-chu", method = RequestMethod.GET)
 	public ModelAndView homePage() {
